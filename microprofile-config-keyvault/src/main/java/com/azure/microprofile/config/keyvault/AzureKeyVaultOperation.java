71dd9b51f61c94a152337b298ae73d8155de8b55
Jianguo Ma <jiangma@microsoft.com>
2023-07-25 15:27:32 +0800

    set app id
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- microprofile-config-keyvault/src/main/java/com/azure/microprofile/config/keyvault/AzureKeyVaultOperation.java
+++ microprofile-config-keyvault/src/main/java/com/azure/microprofile/config/keyvault/AzureKeyVaultOperation.java
@@ -1,5 +1,6 @@
 package com.azure.microprofile.config.keyvault;
 
+import com.azure.core.util.ClientOptions;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
@@ -8,10 +9,14 @@ import java.util.Map;
 import java.util.Set;
 
 interface AzureKeyVaultOperation {
+    String AZURE_MICROPROFILE_KEY_VAULT_SECRETS = "az-mp-kv-secrets";
 
     static SecretClient defaultSecretKeyVaultClient(String url) {
-        return new SecretClientBuilder().vaultUrl(url)
-                .credential(new DefaultAzureCredentialBuilder().build()).buildClient();
+        return new SecretClientBuilder()
+                .clientOptions(new ClientOptions().setApplicationId(AZURE_MICROPROFILE_KEY_VAULT_SECRETS))
+                .vaultUrl(url)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
     }
 
     Set<String> getPropertyNames();
