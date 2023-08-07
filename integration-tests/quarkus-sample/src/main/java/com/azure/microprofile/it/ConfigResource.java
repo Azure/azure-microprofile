4bf4d6788f8d1945227440c337f39299e6001598
Jianguo Ma <jiangma@microsoft.com>
2023-08-07 08:56:16 +0800

    refine quarkus sample for integration test
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- integration-tests/quarkus-sample/src/main/java/com/azure/microprofile/it/ConfigResource.java
+++ integration-tests/quarkus-sample/src/main/java/com/azure/microprofile/it/ConfigResource.java
@@ -2,27 +2,21 @@ package com.azure.microprofile.it;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.Config;
 
 @Path("/config")
 public class ConfigResource {
 
     @Inject
-    ConfigService configService;
+    private Config config;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("{name}")
+    @Path("/{name}")
     public String getConfigValue(String name) {
-        if (name.equals("secret")) {
-            return configService.getSecret();
-        } else if (name.equals("anotherSecret")) {
-            return configService.getAnotherSecret();
-        } else {
-            throw new NotFoundException("Not found for " + name);
-        }
+        return config.getConfigValue(name).getValue();
     }
 }
--- integration-tests/quarkus-sample/src/test/java/com/azure/microprofile/it/ConfigResourceIT.java
+++ integration-tests/quarkus-sample/src/test/java/com/azure/microprofile/it/ConfigResourceIT.java
@@ -1,9 +1,37 @@
 package com.azure.microprofile.it;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
-public class ConfigResourceIT extends ConfigResourceTest {
+@EnabledIfSystemProperty(named = "azure.test", matches = "true")
+public class ConfigResourceIT {
 
-    // Execute the same tests but in native mode.
+    @Test
+    public void testConfigResource() {
+        given()
+                .when().get("/config/secret")
+                .then()
+                .statusCode(200)
+                .body(is("1234"));
+
+        given()
+                .when().get("/config/anotherSecret")
+                .then()
+                .statusCode(200)
+                .body(is("5678"));
+
+        given()
+                .pathParam("name", UUID.randomUUID().toString())
+                .when().get("/config/{name}")
+                .then()
+                .statusCode(204);
+    }
+
 }
